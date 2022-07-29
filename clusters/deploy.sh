0fd7a60738d6c79335bb8ea6776a144ca8638c8d
Esten Rye <esten.rye@ryezone.com>
2022-07-29 11:25:56 -0500

    refactor cluster provisioning to use kustomize

---
+++ clusters/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+SCRIPT_DIR=`cd "$(dirname $0)" && pwd`
+
+PROJECT='default'
+
+if [ $# -ne 1 ]; then
+  echo "No cluster name provided."
+  exit 1
+fi
+
+APP_NAME=$1
+DEST_SERVER=`jq -r ".${APP_NAME}.dest-server" ${SCRIPT_DIR}/clusters.json`
+PROJECT=`jq -r ".${APP_NAME}.project" ${SCRIPT_DIR}/clusters.json`
+
+argocd app create cluster-${APP_NAME} \
+  --project ${PROJECT} \
+  --repo git@github .com:estenrye/cd-homelab.git \
+  --path clusters/overlays/${APP_NAME} \
+  --dest-server "${DEST_SERVER}"
+  --upsert
