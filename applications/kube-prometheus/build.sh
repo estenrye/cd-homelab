e7a279f3227c13b9cb57003fedb32ab4bf294eb1
Esten Rye <esten.rye@ryezone.com>
2023-04-01 21:49:02 -0500

    working general dashboard

--- applications/kube-prometheus/build.sh
+++ applications/kube-prometheus/build.sh
@@ -16,34 +16,6 @@ PATH="$SCRIPT_DIR/tmp/bin:${PATH}"
 # Download Dashboard Updates
 wget https://grafana.com/api/dashboards/3070/revisions/3/download -O "$SCRIPT_DIR/dashboards/etcd.json"
 
-wget https://raw.githubusercontent.com/sladkoff/minecraft-prometheus-exporter/master/dashboards/minecraft-general-dashboard.json -O "$SCRIPT_DIR/dashboards/minecraft-general-dashboard.json"
-
-jq '.__inputs = [ {"name": "DS_PROMETHEUS", "label": "prometheus", "description": "", "type": "datasource", "pluginId": "prometheus", "pluginName": "Prometheus" } ]' \
-   "$SCRIPT_DIR/dashboards/minecraft-general-dashboard.json" \
-   | tee "$SCRIPT_DIR/dashboards/minecraft-general-dashboard-1.json" \
-   && mv "$SCRIPT_DIR/dashboards/minecraft-general-dashboard-1.json" "$SCRIPT_DIR/dashboards/minecraft-general-dashboard.json"
-
-jq '.panels[].datasource = { "type": "prometheus", "uid": "${DS_PROMETHEUS}" }' \
-   "$SCRIPT_DIR/dashboards/minecraft-general-dashboard.json" \
-   | tee "$SCRIPT_DIR/dashboards/minecraft-general-dashboard-1.json" \
-   && mv "$SCRIPT_DIR/dashboards/minecraft-general-dashboard-1.json" "$SCRIPT_DIR/dashboards/minecraft-general-dashboard.json"
-
-jq '.panels[].targets[].datasource = { "type": "prometheus", "uid": "${DS_PROMETHEUS}" }' \
-   "$SCRIPT_DIR/dashboards/minecraft-general-dashboard.json" \
-   | tee "$SCRIPT_DIR/dashboards/minecraft-general-dashboard-1.json" \
-   && mv "$SCRIPT_DIR/dashboards/minecraft-general-dashboard-1.json" "$SCRIPT_DIR/dashboards/minecraft-general-dashboard.json"
-
-jq '.templating.list[].datasource = { "type": "prometheus", "uid": "${DS_PROMETHEUS}" }' \
-   "$SCRIPT_DIR/dashboards/minecraft-general-dashboard.json" \
-   | tee "$SCRIPT_DIR/dashboards/minecraft-general-dashboard-1.json" \
-   && mv "$SCRIPT_DIR/dashboards/minecraft-general-dashboard-1.json" "$SCRIPT_DIR/dashboards/minecraft-general-dashboard.json"
-
-jq '.templating.list[0].query.query = "label_values(mc_players_total, job)"' \
-   "$SCRIPT_DIR/dashboards/minecraft-general-dashboard.json" \
-   | tee "$SCRIPT_DIR/dashboards/minecraft-general-dashboard-1.json" \
-   && mv "$SCRIPT_DIR/dashboards/minecraft-general-dashboard-1.json" "$SCRIPT_DIR/dashboards/minecraft-general-dashboard.json"
-
-
 wget https://raw.githubusercontent.com/sladkoff/minecraft-prometheus-exporter/master/dashboards/minecraft-players-dashboard.json -O "$SCRIPT_DIR/dashboards/minecraft-players-dashboard.json"
 wget https://raw.githubusercontent.com/sladkoff/minecraft-prometheus-exporter/master/dashboards/minecraft-server-dashboard.json -O "$SCRIPT_DIR/dashboards/minecraft-server-dashboard.json"
 
