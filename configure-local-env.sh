ba66ee9425526cd2c0a7894e4730dfc9a7a576de
Esten Rye <esten.rye@ryezone.com>
2023-05-28 18:50:31 -0500

    remove unused files

--- configure-local-env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(pyenv init -)"' >> ~/.zshrc
