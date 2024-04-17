c5932a850fbfe7c5c1f2bbae9b5b72bb9ac8a1d1
Esten Rye <esten.rye@ryezone.com>
2024-04-17 03:26:46 -0500

    add tcproutes dashboard

--- infrastructure/rackspace-spot/remote-backend/tf-workload/gateway-api-examples.tf
+++ infrastructure/rackspace-spot/remote-backend/tf-workload/gateway-api-examples.tf
@@ -11,7 +11,7 @@ resource "kubernetes_deployment_v1" "yages" {
 
   metadata {
     name = "yages"
-    namespace = kubernetes_namespace.gateway_api_examples.0.metadata.0.name
+    namespace = resource.kubernetes_namespace.gateway_api_examples.0.metadata.0.name
     labels = {
       app = "yages"
       example = "grpc-routing"
@@ -54,7 +54,7 @@ resource "kubernetes_service" "yages" {
   
   metadata {
     name = "yages"
-    namespace = kubernetes_namespace.gateway_api_examples.0.metadata.0.name
+    namespace = resource.kubernetes_namespace.gateway_api_examples.0.metadata.0.name
     labels = {
       app = "yages"
       example = "grpc-routing"
@@ -143,6 +143,159 @@ resource "kubernetes_manifest" "grpcroute_yages" {
             },
           ]
         },
+      ]
+    }
+  }
+}
+
+resource "kubernetes_deployment_v1" "echo_basic" {
+  count = var.deploy_gateway_api_examples ? 1 : 0
+
+  metadata {
+    name = "echo-basic"
+    namespace = resource.kubernetes_namespace.gateway_api_examples.0.metadata.0.name
+    labels = {
+      app = "echo-basic"
+      example = "tcp-routing"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "echo-basic"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "echo-basic"
+        }
+      }
+
+      spec {
+        container {
+          name = "backend"
+          image = "gcr.io/k8s-staging-gateway-api/echo-basic:v20231214-v1.0.0-140-gf544a46e"
+          port {
+            name = "echo"
+            container_port = 3000
+            protocol = "TCP"
+          }
+          env {
+            name = "POD_NAME"
+            value_from {
+              field_ref {
+                field_path = "metadata.name"
+              }
+            }
+          }
+          env {
+            name = "POD_NAMESPACE"
+            value_from {
+              field_ref {
+                field_path = "metadata.namespace"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service_v1" "echo_basic" {
+  count = var.deploy_gateway_api_examples ? 1 : 0
+
+  metadata {
+    name = "echo-basic"
+    namespace = resource.kubernetes_namespace.gateway_api_examples.0.metadata.0.name
+    labels = {
+      app = "echo-basic"
+      example = "tcp-routing"
+    }
+  }
+  
+  spec {
+    selector = {
+      app = "echo-basic"
+    }
+
+    type = "ClusterIP"
+
+    port {
+      name = "echo"
+      port = 3000
+      protocol = "TCP"
+      target_port = "echo"
+    }
+  }
+}
+
+resource "kubernetes_manifest" "example_gateway" {
+  count = var.deploy_gateway_api_examples ? 1 : 0
+
+  manifest = {
+    apiVersion = "gateway.networking.k8s.io/v1"
+    kind = "Gateway"
+    metadata = {
+      name = "example-gateway"
+      namespace = resource.kubernetes_namespace.gateway_api_examples.0.metadata.0.name
+    }
+    spec = {
+      gatewayClassName = "eg"
+      listeners = [
+        {
+          name = "foo"
+          protocol = "TCP"
+          port = 8888
+          allowedRoutes = {
+            kinds = [
+              {
+                kind = "TCPRoute"
+              }
+            ]
+          }
+        }
+      ]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "tcproute_echo_basic" {
+  count = var.deploy_gateway_api_examples ? 1 : 0
+
+  manifest = {
+    apiVersion = "gateway.networking.k8s.io/v1alpha2"
+    kind = "TCPRoute"
+    metadata = {
+      name = "echo-basic"
+      namespace = resource.kubernetes_namespace.gateway_api_examples.0.metadata.0.name
+    }
+    spec = {
+      parentRefs = [
+        {
+          name = "example-gateway"
+          group = "gateway.networking.k8s.io"
+          kind = "Gateway"
+          sectionName = "foo"
+        }
+      ]
+      rules = [
+        {
+          backendRefs = [
+            {
+              group = ""
+              kind = "Service"
+              name = "echo-basic"
+              port = 3000
+              weight = 1
+            }
+          ]
+        }
       ]
     }
   }
