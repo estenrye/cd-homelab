a6af17326339dbef79b13b91953cb9a2f1b23b89
Esten Rye <esten.rye@ryezone.com>
2024-10-18 16:44:22 -0500

    working purge-state script

--- terraform/scripts/terragrunt-state/purge-state.sh
+++ terraform/scripts/terragrunt-state/purge-state.sh
@@ -17,11 +17,11 @@ if [ -z "TG_S3_BUCKET_URI" ]; then
   echo "TG_S3_BUCKET_URI is not set. Exiting."
   exit 1
 fi
-echo '{"LockID": { "S": "'${TG_DYNAMODB_TABLE}'/'${TG_PATH}'/terraform.tfstate-md5"}}'
 echo "Purging state for ${TG_S3_BUCKET_URI}/${TG_PATH}/terraform.tfstate"
-aws --profile ${TG_AWS_PROFILE} s3 rm ${TG_S3_BUCKET_URI}/${TG_PATH}/terraform.tfstate
-aws --profile ${TG_AWS_PROFILE} dynamodb delete-item \
-  --table-name "${TG_DYNAMODB_TABLE}" \
-  --key='{"LockID": { "S": "'${TG_DYNAMODB_TABLE}'/'${TG_PATH}'/terraform.tfstate-md5"}}'
 
-"${TG_DYNAMODB_TABLE}/${TG_PATH}/terraform.tfstate-md5"
+aws --profile ${TG_AWS_PROFILE} s3 rm \
+  "s3://${TG_S3_BUCKET_NAME}/${TG_PATH}/terraform.tfstate"
+
+aws --profile ${TG_AWS_PROFILE} dynamodb delete-item \
+  --table=${TG_DYNAMODB_TABLE} \
+  --key="{\"LockID\":{\"S\":\"${TG_S3_BUCKET_NAME}/${TG_PATH}/terraform.tfstate-md5\"}}"
