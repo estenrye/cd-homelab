3dd603ed08494e3138fbe0861d274b68e0de7ccd
Esten Rye <esten.rye@ryezone.com>
2025-02-23 16:19:18 -0600

    add download logic for bin files.

--- transformers/envsubst/kustomize-krm-envsubst.sh
+++ transformers/envsubst/kustomize-krm-envsubst.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+KRM_VERSION="v0.10.0"
 KRM_OS=`uname -o`
 KRM_ARCH=`uname -m`
 
-KUSTOMIZE_KRM_ENVSUBST="./kustomize-krm-envsubst-${KRM_OS}-${KRM_ARCH}"
+KUSTOMIZE_KRM_ENVSUBST="$SCRIPT_DIR/bin/kustomize-krm-envsubst"
 
-$SCRIPT_DIR/bin/$KUSTOMIZE_KRM_ENVSUBST $@
+if [ -f $SCRIPT_DIR/bin/$KUSTOMIZE_KRM_ENVSUBST ]; then
+  $SCRIPT_DIR/bin/$KUSTOMIZE_KRM_ENVSUBST $@
+  curl \
+    -Lo $SCRIPT_DIR/bin/krm_envsubst.tar.gz \
+    https://github.com/logandavies181/kustomize-krm-envsubst/releases/download/${KRM_VERSION}/kustomize-krm-envsubst_${KRM_OS}_${KRM_ARCH}.tar.gz
+  cd $SCRIPT_DIR/bin
+  tar -xvf $SCRIPT_DIR/bin/krm_envsubst.tar.gz -C $SCRIPT_DIR/bin
+fi
+
+$KUSTOMIZE_KRM_ENVSUBST $@
